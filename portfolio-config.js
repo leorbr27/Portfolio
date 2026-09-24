21461498046368b071ecc2372f36c6b13fddaa6e
leorbr27 <leorbr27@gmail.com>
2026-09-23 22:20:26 -0300

    Configurar foto de perfil local

--- portfolio-config.js
+++ portfolio-config.js
@@ -16,7 +16,7 @@
 // ============================================================
 
 const PORTFOLIO_CONFIG = {
-  profilePhoto: "",
+  profilePhoto: "perfil.jpeg",
 
   projectImages: {
     Supermercado: "https://instagram.fvix24-1.fna.fbcdn.net/v/t51.82787-15/810022934_18629791687042598_5873124617483903863_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=111&ig_cache_key=Mzk4NTgxMTA1MDk2NDM1NjExNg%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6IkNBUk9VU0VMX0lURU0ueHBpZHMuMzA3Mi5zZHIucmVndWxhcl9waG90by5DMyJ9&_nc_ohc=-x1FlBaRXvsQ7kNvwE25-Ls&_nc_oc=AdoZTbyprUbewSniN19qq4JrR3KblgqGGbPSgHnMLiWtsOOxFlrskjY4iqJLl7uy-LgU_mXLEAseKJCcX0Ktyo_U&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fvix24-1.fna&_nc_gid=3i4DXBQcF81IQHMhC4OcWQ&_nc_ss=7a22e&oh=00_AQJmLnVTUQ-MkOpbgDRykAU-4kNBgS9CYGPWlukyb5CdUQ&oe=6ABA5487",
